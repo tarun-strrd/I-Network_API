721dfff213ad10e95448da09f3ffa16d817a79ce
tarun-strrd <tarunsayyapureddi123@gmail.com>
2023-08-24 04:39:49 +0530

    updated cors origin url

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ mongoose.connection.on("connected", () => {
 });
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://ivy-kids-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
